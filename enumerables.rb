9e57a62ccecf23deda1a9afe44dd65fc267ef4a7
rebeccamrfoster <rebeccamrfoster@gmail.com>
2021-08-23 15:46:40 -0400

    my_rotate

--- enumerables.rb
+++ enumerables.rb
@@ -54,14 +54,49 @@ class Array
     end
 
     def my_zip(*args)
-        result = Array.new(self.length){Array.new(args.length + 1)}
+        result = Array.new(self.length) { [] }
         args.unshift(self)
 
+        (0...self.length).to_a.my_each do |i|
+            args.my_each do |arg|
+                result[i] << arg[i]
+            end
+        end
+
+        result
+    end
+
+    def my_rotate(n=1)
+        rotations = n % self.length
+        dup = self.dup
+
+        rotations.times do
+            dup.push(dup.shift)
+        end
+
+        dup
     end
 
 
 end
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
+
+a = [ "a", "b", "c", "d" ]
+p a.my_rotate         #=> ["b", "c", "d", "a"]
+p a.my_rotate(2)      #=> ["c", "d", "a", "b"]
+p a.my_rotate(-3)     #=> ["b", "c", "d", "a"]
+p a.my_rotate(15)     #=> ["d", "a", "b", "c"]
+
+# a = [ 4, 5, 6 ]
+# b = [ 7, 8, 9 ]
+# p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+# p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+# p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+# c = [10, 11, 12]
+# d = [13, 14, 15]
+# p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
+
+# p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten
 # a = [1, 2, 3]
 # p a.my_any? { |num| num > 1 } # => true
 # p a.my_any? { |num| num == 4 } # => false
